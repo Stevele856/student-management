d18234dbd843e56b97500c237a513f7c25cddbb0
Steve <letrongvu.work@gmail.com>
2026-04-07 21:58:10 +0700

    test: normalizeStudent and AddStudent

--- internal/services/student_services_mock_test.go
+++ internal/services/student_services_mock_test.go
@@ -1,4 +1,4 @@
-package services_test
+package services
 
 import (
 	"github.com/student-management/internal/models"
--- internal/services/student_services_test.go
+++ internal/services/student_services_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -172,6 +173,199 @@ func TestValidateStudent(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			err := validateStudent(tc.student)
 			if err != tc.expectedErr {
+				t.Errorf("[%s] got %v, want %v", tc.name, err, tc.expectedErr)
+			}
+		})
+	}
+}
+
+func TestNormalizeStudent(t *testing.T) {
+	expectedStudent := &models.Student{
+		FullName:    "Nguyen Van A",
+		DateOfBirth: time.Now().AddDate(-26, 2, 3),
+		Gender:      "male",
+		Address:     "Ho Chi Minh City",
+		Class:       "4A",
+		Email:       "vult@gmail.com",
+		Scores: []*models.SubjectScore{
+			{
+				Subject: "Toan",
+				Score:   6.5,
+			},
+		},
+	}
+
+	tests := []struct {
+		name                string
+		input               *models.Student
+		expectedStudentData *models.Student
+	}{
+		{
+			name: "trim space in full name",
+			input: &models.Student{
+				FullName:    "   Nguyen Van A ",
+				DateOfBirth: time.Now().AddDate(-26, 2, 3),
+				Gender:      "male",
+				Address:     "Ho Chi Minh City",
+				Class:       "4A",
+				Email:       "vult@gmail.com",
+				Scores: []*models.SubjectScore{
+					{
+						Subject: "Toan",
+						Score:   6.5,
+					},
+				},
+			},
+			expectedStudentData: expectedStudent,
+		},
+		{
+			name: "lowercase and trim email",
+			input: &models.Student{
+				FullName:    "Nguyen Van A",
+				DateOfBirth: time.Now().AddDate(-26, 2, 3),
+				Gender:      "male",
+				Address:     "Ho Chi Minh City",
+				Class:       "4A",
+				Email:       " vUlt@GmaiL.com ",
+				Scores: []*models.SubjectScore{
+					{
+						Subject: "Toan",
+						Score:   6.5,
+					},
+				},
+			},
+			expectedStudentData: expectedStudent,
+		},
+		{
+			name: "trim class",
+			input: &models.Student{
+				FullName:    "Nguyen Van A",
+				DateOfBirth: time.Now().AddDate(-26, 2, 3),
+				Gender:      "male",
+				Address:     "Ho Chi Minh City",
+				Class:       " 4A ",
+				Email:       " vult@gmaiL.com ",
+				Scores: []*models.SubjectScore{
+					{
+						Subject: "Toan",
+						Score:   6.5,
+					},
+				},
+			},
+			expectedStudentData: expectedStudent,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			originalFullname := tc.input.FullName
+			orginalEmail := tc.input.Email
+			originalClass := tc.input.Class
+
+			result := normalizeStudent(tc.input)
+
+			if result.FullName != tc.expectedStudentData.FullName {
+				t.Errorf("Fullname: got %q, want %q", result.FullName, tc.expectedStudentData.FullName)
+			}
+
+			if result.Email != tc.expectedStudentData.Email {
+				t.Errorf("Email: got %q, want %q", result.Email, tc.expectedStudentData.Email)
+			}
+
+			if result.Class != tc.expectedStudentData.Class {
+				t.Errorf("Class: got %q, want %q", result.Class, tc.expectedStudentData.Class)
+			}
+
+			// make sure input does not being mutated
+			if tc.input.FullName != originalFullname {
+				t.Errorf("input.FullName bị mutate: got %q, want %q", tc.input.FullName, tc.input.FullName)
+			}
+			if tc.input.Email != orginalEmail {
+				t.Errorf("input.Email bị mutate: got %q, want %q", tc.input.Email, tc.expectedStudentData.Email)
+			}
+			if tc.input.Class != originalClass {
+				t.Errorf("input.Class bị mutate: got %q, want %q", tc.input.Class, tc.expectedStudentData.Class)
+			}
+
+			// Kiểm tra trả về pointer mới
+			if result == tc.input {
+				t.Error("normalizeStudent() trả về cùng pointer với input")
+			}
+		})
+	}
+
+}
+
+func TestAddStudent(t *testing.T) {
+	validStudent := &models.Student{
+		FullName:    "Nguyen Van A",
+		DateOfBirth: time.Now().AddDate(-26, 2, 3),
+		Gender:      "male",
+		Address:     "Ho Chi Minh City",
+		Class:       "4A",
+		Email:       "vult@gmail.com",
+		Scores: []*models.SubjectScore{
+			{
+				Subject: "Toan",
+				Score:   6.5,
+			},
+		},
+	}
+
+	tests := []struct {
+		name        string
+		input       *models.Student
+		mockRepo    *MockStudentRepository
+		expectedErr error
+	}{
+		{
+			name:        "nil student",
+			input:       nil,
+			mockRepo:    &MockStudentRepository{},
+			expectedErr: ErrStudentData,
+		},
+		{
+			name:        "validation failed",
+			input:       &models.Student{},
+			mockRepo:    &MockStudentRepository{},
+			expectedErr: ErrNameRequired,
+		},
+		{
+			name:  "email already existed",
+			input: validStudent,
+			mockRepo: &MockStudentRepository{
+				GetStudentByEmailFn: func(email string) (*models.Student, error) {
+					return &models.Student{Email: email}, nil
+				},
+			},
+			expectedErr: ErrEmailExisted,
+		},
+		{
+			name:  "success create student",
+			input: validStudent,
+			mockRepo: &MockStudentRepository{
+				GetStudentByEmailFn: func(email string) (*models.Student, error) {
+					return nil, errors.New("not found")
+				},
+				AddStudentFn: func(student *models.Student) error {
+					if student.ID == "" {
+						t.Errorf("expected ID to be generated")
+					}
+					return nil
+				},
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			service := &StudentService{
+				repo: tc.mockRepo,
+			}
+
+			err := service.AddStudent(tc.input)
+
+			if !errors.Is(err, tc.expectedErr) {
 				t.Errorf("[%s] got %v, want %v", tc.name, err, tc.expectedErr)
 			}
 		})
